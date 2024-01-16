805d4b7c85fa3424dde33a46865ae37e521d6a78
Adedax39 <lasathrathnayake@gmail.com>
2024-01-16 16:30:30 +0530

    FrontEnd Done

--- src/MyCrud.js
+++ src/MyCrud.js
@@ -1,84 +1,160 @@
-import React, {useState, useEffect} from 'react';
-import Table from 'react-bootstrap/Table';
-import { Fragment } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'bootstrap';
+import React, { useState, useEffect } from "react";
+import Table from "react-bootstrap/Table";
+import { Fragment } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
 
-const CRUD = () =>{
+const CRUD = () => {
+  const [show, setShow] = useState(false);
 
-const empdata = [
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [name,setName] = useState("");
+  const [age,setAge] = useState("");
+  const [isActive,setisActive] = useState(0);
+
+  const[editId,setEditId] = useState("");
+  const [editName,editSetName] = useState("");
+  const [editAge,editSetAge] = useState("");
+  const [editIsActive,editSetisActive] = useState(0);
+
+
+  const empdata = [
     {
-      id : 1,
-      name : "Bruce",
-      age :26,
-      isActive : 1
+      id: 1,
+      name: "Bruce",
+      age: 26,
+      isActive: 1,
     },
     {
-        id : 2,
-        name : "Tony",
-        age :36,
-        isActive : 1
-
+      id: 2,
+      name: "Tony",
+      age: 36,
+      isActive: 1,
     },
     {
-        id : 3,
-        name : "Alfred",
-        age :76,
-        isActive : 0
-      }
-]
- const handleEdit = (id) => {
-     alert(id);
- }
+      id: 3,
+      name: "Alfred",
+      age: 76,
+      isActive: 0,
+    },
+  ];
+  const handleEdit = (id) => {
+    //alert(id);
+    handleShow();
+  };
 
- const handleDelete = (id) => {
+  const handleDelete = (id) => {
+    if (window.confirm("Are you to delete this employee?") === true) 
     alert(id);
-}
+  };
+
+  const handleUpdate = () => {
+    
+  };
 
 
+  const [data, setData] = useState([]);
 
-    const [data,setData] = useState([])
-
-    useEffect(()=>(
-        setData(empdata)
-     ),[])
-    return(
+  useEffect(() => setData(empdata), []);
+  return (
     <Fragment>
+      <Container>
+        <Row>
+          <Col>
+            <input type="text" className="form-control" placeholder="Enter Name" onChange={(e) =>setName(e.target.value)} value={name} />
+          </Col>
+          <Col>
+          <input type="text" className="form-control" placeholder="Enter Age" onChange={(e) =>setAge(e.target.value)} value={age}/>
+          </Col>
+          <Col>
+          <input type="checkbox"
+           checked={isActive === 1 ? true : false}
+           onChange={(e) => setisActive(e)} value={isActive}
+          />
+          <label>IsActive</label>
+          </Col>
+          <Col>
+            <button className="btn btn-primary">Submit</button>
+          </Col>
+        </Row>
+      </Container>
+      <br />
       <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Age</th>
-          <th>IsActive</th>
-          <th>Actions</th>
-
-        </tr>
-      </thead>
-      <tbody>
-       {
-         data && data.length > 0 ?
-         (data.map((item, index) =>(
-         <tr key={index}>
-          <td>{index+ 1}</td>
-          <td>{item.id}</td>
-          <td>{item.name}</td>
-          <td>{item.age}</td>
-          <td>{item.isActive}</td>
-          <td colSpan={2}>
-            <button className = "btn btn-primary" onClick={() =>handleEdit(item.id)}>Edit</button> &nbsp;          
-            <button className = "btn btn-danger" onClick={() =>handleDelete(item.id)}>Delete</button>
-          </td>
-        </tr>
-         )))
-         :
-         "Loading..."
-       }        
-       </tbody>
-     </Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>IsActive</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data && data.length > 0
+            ? data.map((item, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item.name}</td>
+                  <td>{item.age}</td>
+                  <td>{item.isActive}</td>
+                  <td colSpan={2}>
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => handleEdit(item.id)}
+                    >
+                      Edit
+                    </button>{" "}
+                    &nbsp;
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => handleDelete(item.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            : "Loading..."}
+        </tbody>
+      </Table>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Row>
+          <Col>
+            <input type="text" className="form-control" placeholder="Enter Name" onChange={(e) =>editSetName(e.target.value)} value={editName}/>
+          </Col>
+          <Col>
+          <input type="text" className="form-control" placeholder="Enter Age" onChange={(e) =>editSetAge(e.target.value)} value={editAge}/>
+          </Col>
+          <Col>
+          <input type="checkbox"
+          checked={editIsActive === 1 ? true : false}
+          onChange={(e) => editSetisActive(e)} value={editIsActive}
+          />
+          <label>IsActive</label>
+          </Col>
+        </Row> 
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleUpdate()}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Fragment>
-    )
-}
+  );
+};
 
 export default CRUD;
