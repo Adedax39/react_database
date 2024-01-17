58e03460776b94cdba056874fe5d75ca185aa0a0
Adedax39 <lasathrathnayake@gmail.com>
2024-01-17 08:57:55 +0530

    backend connected

--- src/MyCrud.js
+++ src/MyCrud.js
@@ -7,6 +7,7 @@ import Modal from "react-bootstrap/Modal";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import axios from "axios";
 
 const CRUD = () => {
   const [show, setShow] = useState(false);
@@ -14,70 +15,73 @@ const CRUD = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const [name,setName] = useState("");
-  const [age,setAge] = useState("");
-  const [isActive,setisActive] = useState(0);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [isActive, setisActive] = useState(0);
 
-  const[editId,setEditId] = useState("");
-  const [editName,editSetName] = useState("");
-  const [editAge,editSetAge] = useState("");
-  const [editIsActive,editSetisActive] = useState(0);
+  const [editId, setEditId] = useState("");
+  const [editName, editSetName] = useState("");
+  const [editAge, editSetAge] = useState("");
+  const [editIsActive, editSetisActive] = useState(0);
 
-
-  const empdata = [
-    {
-      id: 1,
-      name: "Bruce",
-      age: 26,
-      isActive: 1,
-    },
-    {
-      id: 2,
-      name: "Tony",
-      age: 36,
-      isActive: 1,
-    },
-    {
-      id: 3,
-      name: "Alfred",
-      age: 76,
-      isActive: 0,
-    },
-  ];
   const handleEdit = (id) => {
     //alert(id);
     handleShow();
   };
 
-  const handleDelete = (id) => {
-    if (window.confirm("Are you to delete this employee?") === true) 
-    alert(id);
-  };
-
-  const handleUpdate = () => {
-    
-  };
-
-
   const [data, setData] = useState([]);
 
-  useEffect(() => setData(empdata), []);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    axios
+      .get("https://localhost:7131/api/Employee")
+      .then((result) => {
+        setData(result.data); // Update state with API response
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleDelete = (id) => {
+    if (window.confirm("Are you to delete this employee?") === true) alert(id);
+  };
+
+  const handleUpdate = () => {};
+
   return (
     <Fragment>
       <Container>
         <Row>
           <Col>
-            <input type="text" className="form-control" placeholder="Enter Name" onChange={(e) =>setName(e.target.value)} value={name} />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Name"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
           </Col>
           <Col>
-          <input type="text" className="form-control" placeholder="Enter Age" onChange={(e) =>setAge(e.target.value)} value={age}/>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Age"
+              onChange={(e) => setAge(e.target.value)}
+              value={age}
+            />
           </Col>
           <Col>
-          <input type="checkbox"
-           checked={isActive === 1 ? true : false}
-           onChange={(e) => setisActive(e)} value={isActive}
-          />
-          <label>IsActive</label>
+            <input
+              type="checkbox"
+              checked={isActive === 1 ? true : false}
+              onChange={(e) => setisActive(e)}
+              value={isActive}
+            />
+            <label>IsActive</label>
           </Col>
           <Col>
             <button className="btn btn-primary">Submit</button>
@@ -96,31 +100,35 @@ const CRUD = () => {
           </tr>
         </thead>
         <tbody>
-          {data && data.length > 0
-            ? data.map((item, index) => (
-                <tr key={index}>
-                  <td>{index + 1}</td>
-                  <td>{item.name}</td>
-                  <td>{item.age}</td>
-                  <td>{item.isActive}</td>
-                  <td colSpan={2}>
-                    <button
-                      className="btn btn-primary"
-                      onClick={() => handleEdit(item.id)}
-                    >
-                      Edit
-                    </button>{" "}
-                    &nbsp;
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => handleDelete(item.id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))
-            : "Loading..."}
+          {data && data.length > 0 ? (
+            data.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.isActive}</td>
+                <td colSpan={2}>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleEdit(item.id)}
+                  >
+                    Edit
+                  </button>{" "}
+                  &nbsp;
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(item.id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={5}>Loading...</td>
+            </tr>
+          )}
         </tbody>
       </Table>
       <Modal show={show} onHide={handleClose}>
@@ -128,21 +136,35 @@ const CRUD = () => {
           <Modal.Title>Edit</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        <Row>
-          <Col>
-            <input type="text" className="form-control" placeholder="Enter Name" onChange={(e) =>editSetName(e.target.value)} value={editName}/>
-          </Col>
-          <Col>
-          <input type="text" className="form-control" placeholder="Enter Age" onChange={(e) =>editSetAge(e.target.value)} value={editAge}/>
-          </Col>
-          <Col>
-          <input type="checkbox"
-          checked={editIsActive === 1 ? true : false}
-          onChange={(e) => editSetisActive(e)} value={editIsActive}
-          />
-          <label>IsActive</label>
-          </Col>
-        </Row> 
+          <Row>
+            <Col>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Name"
+                onChange={(e) => editSetName(e.target.value)}
+                value={editName}
+              />
+            </Col>
+            <Col>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter Age"
+                onChange={(e) => editSetAge(e.target.value)}
+                value={editAge}
+              />
+            </Col>
+            <Col>
+              <input
+                type="checkbox"
+                checked={editIsActive === 1 ? true : false}
+                onChange={(e) => editSetisActive(e)}
+                value={editIsActive}
+              />
+              <label>IsActive</label>
+            </Col>
+          </Row>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
